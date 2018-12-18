440eac6e337d7ae8e3d99492de288ed1edf3f327
Saeed Salehian <saeed@upkook.com>
2018-12-18 16:48:30 +0330

    Added support for robots.txt

--- upkook_web/urls/__init__.py
+++ upkook_web/urls/__init__.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
-urlpatterns = []
+urlpatterns = [
+    url(
+        r'^robots\.txt$',
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        name='robots'
+    ),
+]
 
 # This is only needed when using runserver.
 if settings.DEBUG:  # pragma: no cover
