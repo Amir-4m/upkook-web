0ce735f28b4616fc0783b54ef7bed7a9a7239bf5
Saeed Salehian <saeed@upkook.com>
2019-01-29 18:38:39 +0330

    Updated version to 0.1.0

--- upkook_web/__init__.py
+++ upkook_web/__init__.py
@@ -4,6 +4,6 @@
 from __future__ import absolute_import, unicode_literals
 
 __author__ = 'Upkook'
-__version__ = '0.1.0.dev1'
+__version__ = '0.1.0'
 
 DEFAULT_SETTINGS_MODULE = 'upkook_web.settings.web'
