1e0c9c5f959dc3d24bd8bd6fe1b235a2fcf39195
Saeed Salehian <saeed@upkook.com>
2019-06-28 16:01:40 +0430

    Setup for using a single client ID across cached, AMP and non-AMP pages

--- upkook_web/urls/__init__.py
+++ upkook_web/urls/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 from django.conf import settings
-from django.urls import register_converter, path
+from django.urls import register_converter, path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
@@ -17,6 +17,7 @@ register_converter(AMPConverter, 'amp')
 
 urlpatterns = [
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name='robots'),
+    path('amp/', include('django_contrib.amp.urls', namespace='amp')),
 ]
 
 # This is only needed when using runserver.
