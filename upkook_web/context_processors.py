7c6f52997d306dbf5f677e5411a96f209398aeca
Amir <amiri.amir30@yahoo.com>
2020-01-05 13:17:04 +0330

    sing in url changed to dashboard url

--- upkook_web/context_processors.py
+++ upkook_web/context_processors.py
@@ -7,5 +7,6 @@ def upkook_web(request):
     return {
         'IRAN_YEKAN_LICENSE': getattr(settings, 'IRAN_YEKAN_LICENSE'),
         'pure_chat_id': settings.PURE_CHAT_ID,
-        'recaptcha_public_key': settings.DRF_RECAPTCHA_PUBLIC_KEY
+        'recaptcha_public_key': settings.DRF_RECAPTCHA_PUBLIC_KEY,
+        'dashboard_url': settings.DASHBOARD_URL
     }
